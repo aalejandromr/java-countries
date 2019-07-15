afc413686b492ae29c271e7e954b550bcbdc1587
Alejandro Mejia <aalejandro.mreyes@icloud.com>
2019-07-15 16:28:34 -0400

    Adds all and startWith

---
+++ src/main/java/com/lambdaschool/javacountries/CountryChecker.java
@@ -0,0 +1,5 @@
+package com.lambdaschool.javacountries;
+
+public interface CountryChecker {
+    boolean test(Country c);
+}
--- src/main/java/com/lambdaschool/javacountries/JavaCountriesApplication.java
+++ src/main/java/com/lambdaschool/javacountries/JavaCountriesApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaCountriesApplication {
 
+    static CountryList countryList;
     public static void main(String[] args) {
+        countryList = new CountryList();
         SpringApplication.run(JavaCountriesApplication.class, args);
     }
 
